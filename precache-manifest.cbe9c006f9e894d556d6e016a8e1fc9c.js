971547396b5ae05be27965baaad534ed85d5fc84
Shubhankar Saxena <saxena03121999@gmail.com>
2020-10-26 20:50:18 +0530

    Updates

---
+++ precache-manifest.cbe9c006f9e894d556d6e016a8e1fc9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9833126985562a745a44ce4f62bb96de",
+    "url": "/Post/index.html"
+  },
+  {
+    "revision": "ffd487da9b6463bd8646",
+    "url": "/Post/static/css/main.4f5cd613.chunk.css"
+  },
+  {
+    "revision": "516c1770f52968ad3411",
+    "url": "/Post/static/js/2.cbaf2f26.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/Post/static/js/2.cbaf2f26.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffd487da9b6463bd8646",
+    "url": "/Post/static/js/main.62c0dde3.chunk.js"
+  },
+  {
+    "revision": "d3eb3c842a695eb8666e",
+    "url": "/Post/static/js/runtime-main.9256db20.js"
+  }
+]);
